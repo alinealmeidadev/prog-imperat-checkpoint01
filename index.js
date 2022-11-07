1e4f1764fb252334a174716392368054753eae2c
aline <alinealmeidadev@gmail.com>
2022-11-07 20:06:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const prato01 = pipoca;
+let tempo01 = 10;
+
+
+function preparar (prato01, tempo01) {
+    const prato01 = prato;
+
+    if(!prato01) {
+        return console.log ("Prato inexistente");
+    }
+
+    if (tempoCorreto > tempo) {
+        msg = "Tempo insuficiente";
+    }
+
+    else if (tempo > tempoCorreto * 2) {
+        msg = "A comida queimou";
+    }
+
+    else if (tempo > tempoCorreto * 3) {
+        msg = "Kabummm";
+    }
+
+    else {
+        msg = "Prato pronto, bom apetite!!!";
+    }
+
+    return console.log(msg);
+
+    if("pipoca") {
+        return console.log ("Prato inexistente");
+    }
+
+}
